057dcae50ae0d8e2b5faddbe888efb7b19aad557
Murthi Haari <murthyhaari10@gmail.com>
2026-05-29 13:41:43 +0530

    Add files via upload

---
+++ Recursion/productOfDigits.java
@@ -0,0 +1,14 @@
+package com.Recursion;
+
+public class productOfDigits {
+    public static void main(String[] args) {
+        System.out.println(product(2547));
+
+    }
+    static int product(int n){
+        if(n%10==n){
+            return n;
+        }
+        return (n%10)*product(n/10);
+    }
+}
