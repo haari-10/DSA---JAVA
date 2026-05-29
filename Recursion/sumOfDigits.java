ab48b35ee523e6bbaefc68364b8ee2564f146e2b
Murthi Haari <murthyhaari10@gmail.com>
2026-05-29 13:38:43 +0530

    Add files via upload

---
+++ Recursion/sumOfDigits.java
@@ -0,0 +1,14 @@
+package com.Recursion;
+
+public class sumOfDigits {
+    public static void main(String[] args) {
+        System.out.println(sum(1256));
+    }
+    static int sum(int n){
+        if(n==0){
+            return 0;
+
+        }
+        return (n%10)+sum(n/10);
+    }
+}
