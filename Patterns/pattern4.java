b1d251eea09841a747e5da50e311d52f66f00b36
Murthi Haari <murthyhaari10@gmail.com>
2026-06-02 17:57:37 +0530

    Add files via upload

---
+++ Patterns/pattern4.java
@@ -0,0 +1,16 @@
+package com.patterns;
+
+public class pattern4 {
+    public static void main(String[] args) {
+        triangle(5);
+
+    }
+    static void triangle(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
