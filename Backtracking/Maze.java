507a2a5a7e706574b4d4a8655480d469f9df094f
Murthi Haari <murthyhaari10@gmail.com>
2026-06-17 22:46:02 +0530

    Add files via upload

---
+++ Backtracking/Maze.java
@@ -0,0 +1,51 @@
+package com.Recursion;
+
+import java.util.ArrayList;
+
+public class Maze {
+    public static void main(String[] args) {
+        //System.out.println(pathcount(3,3));
+       //paths("",3,3);
+       System.out.println(pathlist("",3,3));
+    }
+    static int pathcount(int r,int c){
+        if(r==1 || c==1){
+            return 1;
+        }
+        int down=pathcount(r-1,c);
+        int right=pathcount(r,c-1);
+        return down+right;
+    }
+    static void paths(String p,int r,int c){
+
+        if(r==1 && c==1){
+            System.out.println(p);
+            return;
+        }
+        if(r>1){
+            paths(p+'D',r-1,c);
+        }
+        if(c>1){
+            paths(p+'R',r,c-1);
+        }
+
+    }
+    static ArrayList<String> pathlist(String p,int r,int c){
+        if(r==1 && c==1){
+            ArrayList<String>list=new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        ArrayList<String>ans=new ArrayList<>();
+        if(r>1){
+            ArrayList<String>left=pathlist(p+'D',r-1,c);
+            ans.addAll(left);
+        }
+        if(c>1){
+            ArrayList<String>right=pathlist(p+'R',r,c-1);
+            ans.addAll(right);
+        }
+
+        return ans;
+    }
+}
